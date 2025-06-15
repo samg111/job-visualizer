1b551494cc69725fb7490bed85b3e8755b2446aa
berrystump <berrystump@protonmail.com>
2025-06-15 13:31:18 -0400

    fixed working directory location

--- pkg/excel/excel.go
+++ pkg/excel/excel.go
@@ -2,14 +2,11 @@ package excel
 
 import (
 	"log"
-	"os"
 
 	"github.com/xuri/excelize/v2"
 )
 
 func OpenExcelFile() *excelize.File {
-	err := os.Chdir("../..")
-	checkError(err)
 	file, err := excelize.OpenFile("JobData.xlsx")
 	checkError(err)
 	return file
@@ -20,40 +17,6 @@ func GetAllRows(file *excelize.File) [][]string {
 	checkError(err)
 	return rows
 }
-
-// func ProcessRows(rows [][]string, allJobData []structs.JobData) []structs.JobData {
-// 	for _, row := range rows[1:] {
-// 		job := structs.JobData{}
-// 		job.CompanyName = row[0]
-// 		job.DatePosted = row[1]
-// 		// job.JobId = row[2]
-// 		job.Country = row[3]
-// 		job.Location = row[4]
-// 		job.Salary = calcSalary(row)
-// 		job.JobTitle = row[9]
-// 		allJobData = append(allJobData, job)
-// 	}
-// 	return allJobData
-// }
-
-// func calcSalary(row []string) int {
-// 	maxSalaryString := row[6]
-// 	minSalaryString := row[7]
-// 	hourlyOrYearly := row[8]
-
-// 	maxSalary, err := strconv.ParseFloat(maxSalaryString, 64)
-// 	checkError(err)
-// 	// maxSalaryInt := int(maxSalaryFloat)
-// 	minSalary, err := strconv.ParseFloat(minSalaryString, 64)
-// 	checkError(err)
-// 	// minSalaryInt := int(minSalaryFloat)
-// 	salary := int((maxSalary + minSalary) / 2)
-// 	if hourlyOrYearly == "hourly" {
-// 		salary = salary * 40 * 50
-// 	}
-// 	return salary
-
-// }
 
 func checkError(err error) {
 	if err != nil {
